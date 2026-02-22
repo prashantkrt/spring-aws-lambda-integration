faa7ad055bc28116d6614d5265e85d32fe4e9c4a
prashant <prashanttiwary10@gmail.com>
2026-02-22 17:10:03 +0530

    integrate Spring Boot app with AWS Lambda using serverless

--- src/main/java/com/lulu/controller/PingController.java
+++ src/main/java/com/lulu/controller/PingController.java
@@ -1,12 +1,10 @@
 package com.lulu.controller;
 
-
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.HashMap;
 import java.util.Map;
-
 
 @RestController
 @EnableWebMvc
