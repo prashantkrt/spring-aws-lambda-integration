a6a7f30ca371844288ef4bf64c43e57373ef0fc5
prashant <prashanttiwary10@gmail.com>
2026-02-22 17:23:50 +0530

    integrate Spring Boot app with AWS Lambda using serverless

--- src/main/java/com/lulu/exception/ApiErrorResponse.java
+++ src/main/java/com/lulu/exception/ApiErrorResponse.java
@@ -1,5 +1,6 @@
 package com.lulu.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiErrorResponse<T> {
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private LocalDateTime timestamp;
     private int status;
     private String error;
